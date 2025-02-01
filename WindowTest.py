8dabdb1365ec91a612092ff3bf67990e13314df4
Decretum-Wu <314688095@qq.com>
2025-02-01 16:42:23 +0800

    new power logic

--- WindowTest.py
+++ WindowTest.py
@@ -51,6 +51,8 @@ def reset_sub_device():
             print("在设备{0}中，点击进入棋盘".format(gamer.deviceID))
             time.sleep(1)
             break
+        if(gamer.find_pic_touch(rd.start_game)):
+            continue
 
     # 备用机返回主页
     gamer.bs_press('1')
@@ -61,7 +63,7 @@ def save_main_device():
     gamer.bs_press('1')
     time.sleep(3)  # 等待窗口激活
     gamer.find_pic_touch(rd.start_game)
-    time.sleep(5)  # 等待窗口激活
+    time.sleep(2)  # 等待窗口激活
     # while True:
     #     time.sleep(0.5)  # 等待窗口激活
     #     if(gamer.verify_pic(rd.init_loading)):
@@ -76,11 +78,11 @@ def resume_main_device(waitSeconds = 3):
     time.sleep(waitSeconds)  # 等待窗口激活
     gamer.find_pic_touch(rd.start_game)
     while True:
-        time.sleep(0.5)  # 等待窗口激活
+        # time.sleep(0.5)  # 等待窗口激活
         if(gamer.verify_pic(rd.cloud_button)):
             time.sleep(3)  # 等待云端数据可选
             gamer.find_pic_touch(rd.cloud_button)
-            time.sleep(5)  # 等待云端数据可选
+            time.sleep(3)  # 等待云端数据可选
             gamer.find_pic_touch(rd.cloud_submit)
         # 点击棋盘后切换页面
         # if(gamer.verify_pic(rd.back_from_board)):
@@ -89,8 +91,10 @@ def resume_main_device(waitSeconds = 3):
         # TODO 云端数据切换
         if(gamer.find_pic_touch(rd.into_board)): 
             print("在设备{0}中，点击进入棋盘".format(gamer.deviceID))
-            time.sleep(1)
+            time.sleep(2.5)
             break
+        if(gamer.find_pic_touch(rd.start_game)):
+            continue
 
 def select_box():
     time.sleep(2)
@@ -104,38 +108,44 @@ def select_box():
 # [脚本从这里开始运行]
 reset_sub_device()
 resume_main_device()
-select_box()
 
 count = len(gamer.find_pic_all(rd.power_4))
 countNow = count
 
 # 获取箱子初始数量
+point = (0,0)
 boxList = gamer.find_pic_all(rd.box_1)
 boxCount = len(boxList)
-point = (0,0)
 if len(boxList) > 0:
     point = boxList[0]
 else:
     raise Exception(f'箱子列表为空')
+errorCount = 0
 
-# 更新箱子列表
-boxList = gamer.find_pic_all(rd.box_1)
-if len(boxList) < boxCount:
-    boxCount = len(boxList)
-    if len(boxList) > 0:
-        point = boxList[0]
-    else:
-        raise Exception(f'箱子列表为空')
-# 选中箱子
-if not gamer.verify_pic(rd.box_2):
-    gamer.touch(point)
-
-# 点击箱子
-time.sleep(0.5)
-gamer.touch(point)
 
 # while gamer.find_pic_touch(rd.box_selected):
-while gamer.find_pic_touch(rd.box_selected):
+while True:
+    # 更新箱子列表
+    boxList = gamer.find_pic_all(rd.box_1)
+    if len(boxList) < boxCount:
+        boxCount = len(boxList)
+        if len(boxList) > 0:
+            point = boxList[0]
+        else:
+            errorCount += 1
+            print("在设备{0}中，体力箱为0，重新判断")
+            if errorCount > 10:
+                raise Exception(f'箱子列表为空')
+            continue
+            
+    # 选中箱子
+    if not gamer.verify_pic(rd.box_2):
+        gamer.touch(point)
+        time.sleep(0.5)
+
+    # 点击箱子
+    gamer.touch(point)
+
     print("在设备{0}中，点击体力箱".format(gamer.deviceID))
     time.sleep(1.5)
     powerList = gamer.find_pic_all(rd.power_4)
@@ -146,19 +156,17 @@ while gamer.find_pic_touch(rd.box_selected):
             # 合成
             gamer.slide((powerList[0], powerList[1]))
             time.sleep(1.5)
-            if not gamer.verify_pic(rd.box_2):
-                gamer.find_pic_touch(rd.box_1)
         # 保存并重置体力瓶数量
         count = len(gamer.find_pic_all(rd.power_4))
         print("在设备{0}中，合成后4级体力瓶个数: {1}".format(gamer.deviceID, count))
         save_main_device()
-        select_box()
     else :
         # 舍弃现有结果
         gamer.bs_press('1')
         # 次设备重置结果
         reset_sub_device()
         resume_main_device(0.5)
-        select_box()
+        # 重置错误次数
+        errorCount = 0
 
 # TODO main_device其他操作
